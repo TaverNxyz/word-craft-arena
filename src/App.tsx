5505c50407df3f55da27010fa0a7da4a740a57b7
Ξsco <voee178@yahoo.com>
2025-12-04 10:50:37 -0600

    Wrap application in AuthProvider and reorganize routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@/contexts/ThemeContext";
+import { AuthProvider } from "@/contexts/AuthContext";
 import { Navigation } from "@/components/Navigation";
 import { VelarixButton } from "@/components/VelarixButton";
 import Index from "./pages/Index";
@@ -15,29 +16,30 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <ThemeProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/statistics" element={
-            <>
-              <Navigation />
-              <Statistics />
-              <VelarixButton />
-            </>
-          } />
-          <Route path="/history" element={
-            <>
-              <Navigation />
-              <History />
-              <VelarixButton />
-            </>
-          } />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <AuthProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/statistics" element={
+              <>
+                <Navigation />
+                <Statistics />
+                <VelarixButton />
+              </>
+            } />
+            <Route path="/history" element={
+              <>
+                <Navigation />
+                <History />
+                <VelarixButton />
+              </>
+            } />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </ThemeProvider>
   </QueryClientProvider>
 );
